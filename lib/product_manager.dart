f8b0c5bf60da5212970653c78b92a2f16d5406dc
Amon Gathoka <waitaamon@gmail.com>
2018-08-30 08:07:39 +0300

    first commit

---
+++ lib/product_manager.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+import './product_control.dart';
+
+class ProductManager extends StatefulWidget {
+
+  final Map<String, String> startingProduct;
+
+  ProductManager({this.startingProduct});
+  @override
+    State<StatefulWidget> createState() {
+      // TODO: implement createState
+      return _ProductManageState();
+    }
+}
+
+class _ProductManageState extends State<ProductManager> {
+  final  List<Map<String, String>> _products = [];
+
+  @override
+  void initState() {
+    if(widget.startingProduct != null) {
+
+      _products.add(widget.startingProduct);
+
+    }
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _addProduct( Map<String, String> product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+  
+  void _deleteProduct(int index) {
+    setState(() {
+      _products.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: ProductControl(_addProduct),
+        ),
+        Expanded(child: Products(_products, deleteProduct: _deleteProduct))
+      ]
+    );
+  }
+}
